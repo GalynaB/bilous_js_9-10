16571c86d0da654484a173e4a0dcecea8fccddd3
Gala <Gala>
2017-03-10 01:05:51 +0200

    carousel, menu

--- js/script.js
+++ js/script.js
@@ -1,63 +1,38 @@
-$(function () {
-
-  var $links = $('.tabs-menu_link');
-  var $tabsContent = $('#content1, #content2, #content3');
-
-  $links.on('click', function(e) {
-    e.preventDefault();
-
-    if ($(this).attr('id') === 'link1') {
-      $links.removeClass('active');
-      $(this).addClass('active');
-
-      $tabsContent.addClass('hide');
-      $(content1).removeClass('hide');
-
-    } else if ($(this).attr('id') === 'link2') {
-        $links.removeClass('active');
-        $(this).addClass('active');
-
-        $tabsContent.addClass('hide');
-        $(content2).removeClass('hide');
-
-    } else if ($(this).attr('id') === 'link3') {
-        $links.removeClass('active');
-        $(this).addClass('active');
-
-        $tabsContent.addClass('hide');
-        $(content3).removeClass('hide');
-      }
+$(function() {
+  //============= slider
+  $('.jcarousel').jcarousel({
+    animation: {
+    duration: 900,
+    easing: 'linear',
+    },
+  })
+  .jcarouselAutoscroll({
+    interval: 6000,
+    target: '+=1',
+    autostart: true
   });
 
-  $(function () {
-
-    var $input = $('#name, #surname, #address');
-    var $help = $('#help-name, #help-surname, #help-address');
-
-    $input.mouseover(function(e) {
-
-      if ($(this).attr('id') === 'name') {
-
-        $help.addClass('hide');
-        $('#help-name').removeClass('hide');
-
-      } else if ($(this).attr('id') === 'surname') {
-
-          $help.addClass('hide');
-          $('#help-surname').removeClass('hide');
-
-      } else if ($(this).attr('id') === 'address') {
-
-          $help.addClass('hide');
-          $('#help-address').removeClass('hide');
-
-      }
-    });
+  //============ navigation
+  $('.jcarousel-prev').jcarouselControl({
+    target: '-=1'
+  });
+  $('.jcarousel-next').jcarouselControl({
+    target: '+=1'
   });
 
-  $('#show-help').click(function() {
-    $('#help-name').removeClass('hide');
-    $('#help-surname').removeClass('hide');
-    $('#help-address').removeClass('hide');
+  //=========== pagination
+  $('.jcarousel-pagination').jcarouselPagination({
+    item: function(page) {
+    return '<a href="#' + page + '">' + page + '</a>';
+    }
   });
+
+  //============== select
+  $('select').niceSelect();
+
+  //============= checkbox
+  var $check = $('.jcheck');
+  $check.on('mousedown', function() {
+  $(this).toggleClass("active");
+  })
 });
